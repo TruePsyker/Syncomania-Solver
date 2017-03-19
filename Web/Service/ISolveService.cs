7d555eac8ba10dce5d9d3a8ed2fb83ddc73e4fec
Udot <vinnikov.alexander@gmail.com>
2017-03-19 23:03:10 +0300

    Extract logic classes to separate file, add web service project

---
+++ Web/Service/ISolveService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Service
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IWebService" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface ISolveService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "solve/{input}")]
+        string Solve( string input );
+    }
+}
---
+++ Web/Service/SolveService.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Service
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "WebService" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы WebService.svc или WebService.svc.cs в обозревателе решений и начните отладку.
+    public class SolveService : ISolveService
+    {
+        public string Solve( string input )
+        {
+            string[] map;
+            if ( TryParseInput( input, out map ) == false )
+                return string.Empty;
+
+            var gm = new SyncomaniaSolver.GameMap();
+
+            gm.LoadMap( map );
+            var gs = gm.Solve_AStar();
+            return "";
+        }
+
+        private bool TryParseInput( string input, out string[] map )
+        {
+            map = new string[0];
+            return false;
+        }
+    }
+}
